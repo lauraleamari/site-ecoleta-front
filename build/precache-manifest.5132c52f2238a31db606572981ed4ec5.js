3c4f8d2dd334334013b941431d9a86d7bdea6976
Laura Leamari <laura.leamari@infracommerce.com.br>
2020-06-25 11:15:14 -0300

    Commit para deploy

---
+++ build/precache-manifest.5132c52f2238a31db606572981ed4ec5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9b5341d00c5d99967782b3eeb7515f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44a545a2880e52ad141c",
+    "url": "/static/css/main.15ed6ca5.chunk.css"
+  },
+  {
+    "revision": "c10719053388db7915dd",
+    "url": "/static/js/2.e9dca4e3.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.e9dca4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a545a2880e52ad141c",
+    "url": "/static/js/main.ce449775.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "abd2ba69bb6bf8469130b703115dd2a2",
+    "url": "/static/media/home-background.abd2ba69.svg"
+  },
+  {
+    "revision": "26434f1f8d4600c2289a91f27afe85a8",
+    "url": "/static/media/logo.26434f1f.svg"
+  }
+]);
